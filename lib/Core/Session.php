ccd8837196a887c94edc2ec6bca3e6945c112b81
Git Techart <git@tchart.ru>
2025-02-04 13:31:31 +0000

    Version 0.5.21

--- lib/Core/Session.php
+++ lib/Core/Session.php
@@ -56,4 +56,95 @@ class Session
 
 		return $result;
 	}
+
+	/**
+	 * Создаёт новую сессию с сохранением данных (массив $_SESSION) текущей сессии
+	 *
+	 * Если удалось переключиться на новую сессию, то возвращает её session_id
+	 * Если что-то случилось и не удалось, то возвращает false
+	 *
+	 * @return boolean|string
+	 */
+	public function regenerateSessionIdUnstableNetworks(): bool|string
+	{
+		$regenStatus = false; // указатель удачно ли поднялась новая сессия
+		$newSessionId = session_create_id(); // id новой сессии
+		$_SESSION['newSessionId'] = $newSessionId; // запомнить id новой сессии в текущую
+		$_SESSION['destroyed'] = time(); // запомнить в текущую сесию когда её надо 100% уничтожить
+
+		session_write_close(); // записать и закрыть текущую сессию
+
+		$wasInStrictMode = false;
+		if (ini_get('session.use_strict_mode') == 1) {
+			// если строгий режим включён, то надо его отключить
+			ini_set('session.use_strict_mode', 0);
+			// и запомнить это, чтобы потом включить опять
+			$wasInStrictMode = true;
+		}
+
+		$oldSessionData = $_SESSION; // запомнить данные старой сессии
+
+		session_id($newSessionId); // установить новый session_id
+		$regenStatus = $this->regenerateSessionStart(); // старт новой сессии с новым запомненным id
+
+		$_SESSION = $oldSessionData; // записать в новую сессию данные старой
+		// в новой сессии эти данные не нужны
+		unset($_SESSION['destroyed']);
+		unset($_SESSION['newSessionId']);
+		unset($oldSessionData);
+
+		session_write_close(); // записать и закрыть текущую сессию (чтобы вызвать ini_set)
+		if ($wasInStrictMode) {
+			@ini_set('session.use_strict_mode', 1); // восстановить строгий режим
+		}
+
+		$regenStatus = $this->regenerateSessionStart(); // возобновляем нужную сессию
+
+		// если удалось переключиться на новую сессию, то возвращает её session_id
+		// если что-то случилось и не удалось, то возвращает false
+		return $regenStatus ? $newSessionId : false;
+	}
+
+	/**
+	 * Проверяет текущую сессию на предмет её актуальности - $_SESSION['destroyed']
+	 * Если подозрение на взлом, то ошибка и возвращает false
+	 * Если устаревшая сессия из-за ошибок сети, то восстанавливает правильную сессию и возвращает true
+	 *
+	 * @return boolean
+	 */
+	public function regenerateSessionStart(): bool
+	{
+		$result = session_start();
+
+		// если текущая сессия помечена как уничтоженная (старая)
+		if (isset($_SESSION['destroyed'])) {
+
+			// если сессия запрашивается спустя 5 минут как она должна быть уничтожена
+			// это подозрительно, возможно плохой человек, делаем что-то для защиты данных юзера
+			if ($_SESSION['destroyed'] + 300 < time()) {
+				// возможно, тут разлогин юзеров
+				// возможно, тут какой-то лог
+				// throw(new DestroyedSessionAccessException); // возможно, выбросим специальную ошибку
+				$result = false;
+			}
+
+			// если сессия запрашивается раньше 5 минут как она должна быть уничтожена
+			// возможно это из-за плохого соединения
+			if ($result) {
+				// пробуем поднять правильную сессию если она указана
+				if (isset($_SESSION['newSessionId'])) {
+					session_write_close(); // записать и закрыть текущую сессию
+					session_id($_SESSION['newSessionId']); // id новой правильной сессии
+					$result = session_start(); // запускаем правильную сессию
+				} else {
+					// нет указателя на новую сессию - какая-то фигня
+					$result = false;
+				}
+			}
+		}
+
+		// если удалось переключиться на новую сессию, то возвращает true
+		// если что-то случилось и не удалось, то возвращает false
+		return $result;
+	}
 }
