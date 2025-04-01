d4ee6c62e8a4d4ce2ed05f2dad471a445382d0d2
Git Techart <git@tchart.ru>
2025-04-01 11:59:46 +0000

    Version 0.6.3

--- lib/Base/Menu/BaseMenu.php
+++ lib/Base/Menu/BaseMenu.php
@@ -31,7 +31,10 @@ abstract class BaseMenu
 	protected $data = [];
 	protected $allowElements = false;
 	protected $modelName = '';
-
+	protected $depth = null;
+	protected $curFlag = '';
+	protected $tree = [];
+	protected $pid = 0;
 
 	public function __construct()
 	{
@@ -45,20 +48,40 @@ abstract class BaseMenu
 	/**
 	 * Проходит по массиву, сравнивая url с полем code
 	 * Выставляет selected true при соответствии поля link с текущим url
-	 *
+	 * 
 	 * @param string $code
-	 * @return array
+	 * @param array $comparable
+	 * @return bool
 	 */
-	protected function setSelectedMenuElement($code)
+	protected function setSelectedMenuElement(string $code, mixed $pid = null, array $comparable = []): bool
 	{
-		if(!empty($code)) {
-			$expUrl = $_SERVER['REQUEST_URI'];
-			$result = str_contains($expUrl, $code);
+		$result = false;
 
-			return $result;
-		} else {
-			return false;
+		if(!empty($code)) {
+			if (!empty($comparable)) {
+				if (!empty($comparable['flag']) && !empty($this->curFlag)) {
+					$result = in_array($this->curFlag, $comparable['flag']);
+				}
+				
+				if (!$result && !empty($comparable['match'])) {
+					if (mb_substr($comparable['match'], mb_strlen($comparable['match']) - 1) === '*') {
+						$startUrl = mb_substr($comparable['match'], 0, mb_strlen($comparable['match']) - 1);
+						$result = strpos($_SERVER['REQUEST_URI'], $startUrl) === 0;
+					}
+				}
+			}
+
+			if (!$result) {
+				$expUrl = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+				$result = $code === $expUrl;
+			} 
+
+			if ($result) {
+				$this->pid = $pid;
+			}
 		}
+
+		return $result;
 	}
 
 	/**
@@ -67,10 +90,11 @@ abstract class BaseMenu
 	 * Принимает в себя массив секций
 	 * Обратить внимание на $this->setSelectedMenuElement($code)
 	 *
+	 * @param string $ibModelName
 	 * @param array $pID
 	 * @return array
 	 */
-	protected function recursionBuildTree(string $ibModelName = '', $pID = null)
+	protected function recursionBuildTree(string $ibModelName = '', $pID = null): array
 	{
 		$branch = [];
 
@@ -109,48 +133,75 @@ abstract class BaseMenu
 		return $branch;
 	}
 
+	public function setSelectedAll($pid = 0) {
+		if ($pid > 0) {
+			if (isset($this->tree[$pid])) {
+				$this->tree[$pid]['selected'] = true;
+
+				if (!empty($this->tree[$pid]['IBLOCK_SECTION_ID']) && intval($this->tree[$pid]['level']) > 1) {
+					$this->setSelectedAll($this->tree[$pid]['IBLOCK_SECTION_ID']);
+				}
+			}
+		}
+	}
+
 	/**
 	 * Возвращает массив иерархически построенного дерева из секций и элементов инфоблока
 	 * Линейный метод ( метод намного легче рукурсивного )
 	 * Принимает в себя массив секций
 	 * Обратить внимание на $this->setSelectedMenuElement($code)
 	 *
+	 * @param string $ibModelName
 	 * @return array
 	 */
-	protected function linearBuildTree(string $ibModelName = '')
+	public function linearBuildTree(string $ibModelName = ''): array
 	{
 		$branch = [];
-		$tree = [];
 
 		foreach ($this->data as $section) {
-			$selected = $this->setSelectedMenuElement($section['CODE']);
+			$currentModel = '';
+			if ($section['selected'] === true) {
+				$selected = $section['selected'];
+			} else {
+				$selected = $this->setSelectedMenuElement($section['SECTION_PAGE_URL'] ?? $section['link'] ?? $section['code'], $section['IBLOCK_SECTION_ID'] ?? '', ['flag' => $section['flag'], 'match' => $section['match']]);
+			}
+
+			if ($this->allowElements && !empty($section['IBLOCK_CODE'])) {
+				foreach (\App::instances('models') as $model) {
+					if ($section['IBLOCK_CODE'] === $model->table) {
+						$currentModel = get_class($model);
+					}
+				}
+			}
 
 			$newSection = [
-				'link' => $section['CODE'],
-				'title' => $section['NAME'],
-				'text' => $section['DESCRIPTION'],
-				'code' => $section['CODE'],
+				'link' => $section['SECTION_PAGE_URL'] ?? $section['link'],
+				'title' => $section['NAME'] ?? $section['title'],
+				'text' => $section['DESCRIPTION'] ?? $section['text'] ?? '',
+				'code' => $this->genCode($section['CODE'] ?? $section['code'] ?? $section['link']),
 				'selected' => $selected,
-				'level' => $section['DEPTH_LEVEL'],
+				'level' => $section['DEPTH_LEVEL'] ?? $section['level'],
 				'IBLOCK_SECTION_ID' => $section['IBLOCK_SECTION_ID'],
 				'ID' => $section['ID'],
-				'elements' => $this->allowElements ? $this->getIBlockElements(
-					$ibModelName,
+				'elements' => $this->allowElements && !empty($section['IBLOCK_CODE']) ? $this->getIBlockElements(
+					!empty($ibModelName) ? $ibModelName : $currentModel,
 					$section['ID'],
-					function($code = '') {
-						return $this->setSelectedMenuElement($code);
+					function($code = '', $sectionId = '', $comp = []) {
+						return $this->setSelectedMenuElement($code, $sectionId ?? '');
 					}) : '',
 			];
 
-			$tree[$section['ID']] = $newSection;
-			$tree[$section['ID']]['sub'] = [];
+			$this->tree[$section['ID']] = $newSection;
+			$this->tree[$section['ID']]['sub'] = [];
 		}
 
-		foreach ($tree as &$node) {
+		$this->setSelectedAll($this->pid);
+
+		foreach ($this->tree as &$node) {
 			if ($node['IBLOCK_SECTION_ID'] == null) {
 				$branch[] = &$node;
 			} else {
-				$parent = &$tree[$node['IBLOCK_SECTION_ID']];
+				$parent = &$this->tree[$node['IBLOCK_SECTION_ID']];
 				$parent['sub'][] = &$node;
 			}
 		}
@@ -158,55 +209,100 @@ abstract class BaseMenu
 		return $branch;
 	}
 
-	protected function getHbElements(string $hbModelName, object $callback)
+	/**
+	 * Получение элементов хайлоадблока для меню
+	 * 
+	 * @param string $hbModelName
+	 * @param object $callback
+	 * @return array
+	*/
+	protected function getHbElements(string $hbModelName, object $callback, array $additionalProps = []): array
 	{
 		$data = [];
 
 		$items = \App::model($hbModelName)->getElements(
-			['UF_LINK', 'UF_TEXT', 'UF_CODE']
+			array_merge(['UF_LINK', 'UF_TEXT', 'UF_TITLE', 'UF_CODE', 'ID'], array_values($additionalProps))
 		);
 		while ($item = $items->fetch()) {
-			$data[] = [
+			$newData = [
+				'ID' => $hbModelName . $item['ID'],
 				'link' => $item['UF_LINK'],
+				'title' => $item['UF_TITLE'],
 				'text' => $item['UF_TEXT'],
 				'code' => $item['UF_CODE'],
-				'selected' => $callback($item['CODE']),
+				'selected' => $callback($item['UF_LINK']),
 			];
+
+			foreach($additionalProps as $key => $prop) {
+				$newData[$key] = $item[$prop];
+			}
+
+			$data[] = $newData;
 		}
 
 		return $data;
 	}
 
-	protected function getIBlockElements(string $ibModelName, string|int $id = '', object $callback)
+	/**
+	 * Получение элементов инфоблока для меню
+	 * 
+	 * @param string $ibModelName
+	 * @param string|int $id
+	 * @param object $callback
+	 * @return array
+	 */
+	public function getIBlockElements(string $ibModelName, string|int $id = '', object $callback, array $additionalProps = []): array
 	{
 		$data = [];
+		$select = array_merge(['PROPERTY_LINK', 'NAME', 'CODE', 'IBLOCK_SECTION_ID', 'DETAIL_PAGE_URL'], array_values($additionalProps));
 
-		$items = \App::model($ibModelName)->getElements(['PROPERTY_LINK', 'NAME', 'CODE', 'IBLOCK_SECTION_ID'], ['IBLOCK_SECTION_ID' => $id], ['LEFT_MARGIN' => 'ASC']);
-		while ($item = $items->fetch()) {
-			$data[] = [
-				'link' => $item['CODE'],
+		$items = \App::model($ibModelName)->getElements($select, ['IBLOCK_SECTION_ID' => $id], ['LEFT_MARGIN' => 'ASC']);
+		while ($item = $items->GetNext()) {
+			$newData = [
+				'link' => $item['DETAIL_PAGE_URL'],
 				'text' => $item['NAME'],
 				'code' => $item['CODE'],
 				'sectionId' => $item['IBLOCK_SECTION_ID'],
-				'selected' => $callback($item['CODE']),
+				'selected' => $callback($item['DETAIL_PAGE_URL'], $item['IBLOCK_SECTION_ID']),
 			];
+
+			foreach($additionalProps as $key => $prop) {
+				$newData[$key] = $item[$prop];
+			}
+
+			$data[] = $newData;
 		}
 
 		return $data;
 	}
 
-	protected function getIBlockSections(string $ibModelName)
+	/**
+	 * Получение секций инфоблока для меню
+	 * 
+	 * @param string $ibModelName
+	 * @param array $additionalProps
+	 * @param int $offset
+	 * @return array
+	 */
+	protected function getIBlockSections(string $ibModelName, array $additionalProps = [], int $offset = 0): array
 	{
 		$data = [];
+		$select = array_merge(['ID', 'CODE', 'NAME', 'DESCRIPTION', 'DEPTH_LEVEL', 'IBLOCK_SECTION_ID', 'SECTION_PAGE_URL', 'IBLOCK_CODE'], array_values($additionalProps));
 
 		$items = \App::model($ibModelName)->getSections(
-			['ID', 'CODE', 'NAME', 'DESCRIPTION', 'DEPTH_LEVEL', 'IBLOCK_SECTION_ID'],
-			[],
+			$select,
+			['<=DEPTH_LEVEL' => $this->depth + $offset],
 			['LEFT_MARGIN' => 'ASC']
 		);
 
-		while ($item = $items->fetch()) {
-			$data[] = $item;
+		while ($item = $items->GetNext()) {
+			$newData = $item;
+
+			foreach($additionalProps as $key => $prop) {
+				$newData[$key] = $item[$prop];
+			}
+
+			$data[] = $newData;
 		}
 
 		return $data;
@@ -216,32 +312,34 @@ abstract class BaseMenu
 	 * Возвращает меню построенное на элементах HighLoadBlock'a
 	 * Принимает в себя массив элементов
 	 *
+	 * @param string $hbModelName
 	 * @return array
 	 */
-	protected function buildMenuByHLBlockElems(string $hbModelName = ''): array
+	protected function buildMenuByHLBlockElems(string $hbModelName = '', array $props = []): array
 	{
 		$curModelName = !empty($hbModelName) ? $hbModelName : $this->modelName;
 		$this->data = $this->getHbElements(
 			$curModelName,
-			function($code = '') {
-				return $this->setSelectedMenuElement($code);
-			}
+			function($code = '', $pid = null) {
+				return $this->setSelectedMenuElement($code, $pid);
+			},
+			$props['addElemProps'] ?? []
 		);
-
 		return $this->data;
 	}
 
-	/**
+	/**	
 	 * Возвращает массив секций и элементов инфоблока
 	 * Вызывает метод у наследованного класса BaseIblockModel
 	 *
+	 * @param string $ibModelName
+	 * @param bool $allowElements
 	 * @return array
-	 */
-	protected function buildMenuByIBlockSections(string $ibModelName = '', bool $allowElements = false): array
+	 */ 
+	protected function buildMenuByIBlockSections(string $ibModelName = '', array $props = []): array
 	{
 		$curModelName = !empty($ibModelName) ? $ibModelName : $this->modelName;
-		$this->allowElements = $allowElements;
-		$this->data = $this->getIBlockSections($curModelName);
+		$this->data = $this->getIBlockSections($curModelName, !empty($props['addSectionProps']) ? $props['addSectionProps'] : []);
 		$this->data = $this->linearBuildTree($curModelName);
 		// $this->data = $this->recursionBuildTree();
 
@@ -252,6 +350,8 @@ abstract class BaseMenu
 	 * Возвращает массив всех элементов инфоблока, приведённый к общему виду
 	 * Для вывода элементов n-уровня, указать в фильтре в модели необходимый уровень
 	 *
+	 * @param string $ibModelName
+	 * @param string|int $id
 	 * @return array
 	 */
 	protected function buildMenuByIBlockElems(string $ibModelName = '', string|int $id = ''): array
@@ -260,10 +360,121 @@ abstract class BaseMenu
 		$this->data = $this->getIBlockElements(
 			$curModelName,
 			$id,
-			function($code = '') {
-				return $this->setSelectedMenuElement($code);
+			function($code = '', $pid = null) {
+				return $this->setSelectedMenuElement($code, $pid);
 			}
 		);
+
+		return $this->data;
+	}
+
+	/**
+	 * Устанавливает глубину добавления разделов в меню
+	 * 
+	 * @param int $depth
+	 * @return object
+	 */
+	public function depth(int $depth): object
+	{
+		$this->depth = $depth;
+
+		return $this;
+	}
+
+	/**
+	 * Включает добавление элементов инфоблоков в меню
+	 * 
+	 * @param bool $allowElements
+	 * @return object
+	 */
+	public function allowElements(bool $allowElements = false): object
+	{
+		$this->allowElements = $allowElements;
+
+		return $this;
+	}
+
+	public function setFlag(string $flag = ''): void
+	{
+		$this->curFlag = $flag;
+	}
+
+	public function genCode(string $code = '')
+	{
+		$code = mb_strtolower(str_replace('/', '', trim($code))) . '_' . rand(100, 1000000);
+
+		return $code;
+	}
+
+	/**
+	 * 
+	 * 
+	 * @param array $menuArray
+	 * @return array
+	 */
+	protected function buildMenuByArray(array $menuArray = []): array
+	{
+		$data = [];
+		$level = "1";
+
+		foreach($menuArray as $pid => $item) {
+			$selectParams = [];
+			$item['flag'] && $selectParams['flag'] = $item['flag'];
+			$item['match'] && $selectParams['match'] = $item['match'];
+			$data['c' . $pid] = [
+				'link' => $item['link'],
+				'title' => $item['title'],
+				'code' => $item['code'],
+				'selected' => $this->setSelectedMenuElement($item['link'], $pid, $selectParams),
+				'level' => $level,
+				'ID' => 'c' . $pid,
+			];
+
+			if (!empty($item['sub'])) {
+				preg_match('/(.+):(.+)/', $item['sub']['data'], $matches);
+
+				if ($matches[1] === 'highloadblock' && !empty($matches[2])) {
+					$elems = $this->getHbElements(
+						$matches[2],
+						function($code = '', $pid = null) {
+							return $this->setSelectedMenuElement($code, $pid);
+						},
+						$item['sub']['addElemProps']
+					);
+
+					$this->pid = 'c' . $pid;
+
+					foreach($elems as $element) {
+						if ($data['c' . $pid]['level'] + 1 <= $this->depth) {
+							$data[$element['ID']] = $element;
+							$data[$element['ID']]['IBLOCK_SECTION_ID'] = 'c' . $pid;
+							$data[$element['ID']]['level'] = (string) ($data[$data[$element['ID']]['IBLOCK_SECTION_ID']]['level'] + 1);
+						}
+					}
+				}
+
+				if ($matches[1] === 'infoblock' && !empty($matches[2])) {
+					$elems = $this->getIBlockSections(
+						$matches[2],
+						!empty($item['sub']['addSectionProps']) ? $item['sub']['addSectionProps'] : [],
+						(int) $data['c' . $pid]['level']
+					);
+
+					foreach($elems as $element) {
+						if ($element['DEPTH_LEVEL'] + 1 <= $this->depth) {
+							$data[$element['ID']] = $element;
+							$data[$element['ID']]['DEPTH_LEVEL'] = (string) ($data[$element['ID']]['DEPTH_LEVEL'] + 1);
+							$data[$element['ID']]['IBLOCK_SECTION_ID'] = $data[$element['ID']]['IBLOCK_SECTION_ID'] ?? 'c' . $pid;
+						}
+					}
+				}
+
+			}
+		}
+
+		$this->data = $data;
+
+		$this->data = $this->linearBuildTree();
 
 		return $this->data;
 	}
